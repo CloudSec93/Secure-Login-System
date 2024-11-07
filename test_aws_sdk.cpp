382fb23a2d088938b3df808f66a95a901a4879a7
harsh.maheshwary@flame.edu.in <harsh.maheshwary@flame.edu.in>
2024-11-07 18:11:10 +0530

    Modified test_aws_sdk

--- test_aws_sdk.cpp
+++ test_aws_sdk.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout<<"Testing Git";
+    // std::cout<<"Testing Git";
+    std::cout<<"Changing the code of this file, to check git status if it can check if the file has been modified.";
 }
